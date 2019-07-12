50deea2fd0b435f8aa99a0ab33096efbad493f7e
nagellack5c <fatsuitsu@gmail.com>
2019-07-13 01:17:46 +0300

    initial commit: basically only reads arguments and calls db_operator

---
+++ wiper.py
@@ -0,0 +1,10 @@
+from argparse import ArgumentParser
+from db_operator import deleter
+
+parser = ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-n", dest="number")
+group.add_argument("-a", dest="wipe", action="store_true")
+args = vars(parser.parse_args())
+
+deleter(args)
