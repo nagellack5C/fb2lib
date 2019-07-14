ac56622fe2a901dda781cccd550592d42f672013
nagellack5c <fatsuitsu@gmail.com>
2019-07-15 01:13:18 +0300

    PEP8

--- seeker.py
+++ seeker.py
@@ -1,10 +1,15 @@
+'''
+Reads command line arguments and calls the seeking function.
+'''
+
+
 from argparse import ArgumentParser
 from db_operator import seek_book
 
 parser = ArgumentParser()
-parser.add_argument("-a", dest="name")
+parser.add_argument("-a", dest="author")
 parser.add_argument("-n", dest="title")
-parser.add_argument("-y", dest="date")
+parser.add_argument("-y", dest="bookdate")
 args = vars(parser.parse_args())
 
 seek_book(args)
