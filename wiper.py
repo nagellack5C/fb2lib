d31133cf4be24e51e077675354341d2324d16ff7
nagellack5c <fatsuitsu@gmail.com>
2019-07-15 17:06:15 +0300

    PEP8

--- wiper.py
+++ wiper.py
@@ -1,15 +1,15 @@
-'''
-Reads command line arguments and calls the deleting function.
-'''
-
-
 from argparse import ArgumentParser
 from db_operator import deleter
 
+desc = '''
+Удаление записи об одной книге или полное очищение БД.
+'''
+
 parser = ArgumentParser()
 group = parser.add_mutually_exclusive_group()
-group.add_argument("-n", dest="number")
-group.add_argument("-a", dest="wipe", action="store_true")
+group.add_argument("-n", dest="number", help="Удалить книгу по номеру")
+group.add_argument("-a", dest="wipe", action="store_true",
+                   help="Удалить все книги")
 args = vars(parser.parse_args())
 
 deleter(args)
